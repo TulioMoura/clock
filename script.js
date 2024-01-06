6f4bf1a9658cd962a8ac6d5194932dd7a4498c7a
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-01-06 02:57:35 +0000

    changed logic of draw to entirely canvas design

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+var canvas = document.getElementById("clock")
+var ctx = canvas.getContext("2d")
 const pontHora= document.getElementById('hora')
 const pontMinuto= document.getElementById('minuto')
 const pontSegundo= document.getElementById('segundos')
 
-pontHora.style.transform = `rotate(90deg)`
-pontMinuto.style.transform = `rotate(90deg)`
-pontSegundo.style.transform = `rotate(90deg)`
+
+const clockSize = 500
 
 function drawtime(){
 const time = new Date(Date.now());
@@ -14,9 +15,84 @@ if(hour>12){
 }
 let min = time.getMinutes();
 let sec = time.getSeconds();
-pontHora.style.transform = `rotate(${(hour * 30)+90 + min*0.6 }deg)`
-pontMinuto.style.transform = `rotate(${(min * 6 )+90}deg)`
-pontSegundo.style.transform = `rotate(${(sec * 6)+90 }deg)`
+
+
+
+
+
+
+ctx.fillStyle = "white"
+ctx.beginPath();
+ctx.moveTo(clockSize/2,clockSize/2)
+ctx.arc(clockSize/2,clockSize/2,(clockSize/100*40),0, Math.PI *2, false)
+ctx.fill();
+ctx.moveTo(0,0)
+let ang = (hour*30 -90) * (Math.PI/180)
+
+let posx = Math.cos(ang)*(clockSize/100*25)+Math.sin(ang)*0;
+let posy = Math.sin(ang)*(clockSize/100*25)+Math.cos(ang)*0;
+
+    posx = posx+(clockSize/2);
+    posy = posy+(clockSize/2);
+    ctx.beginPath()
+    ctx.moveTo(clockSize/2,clockSize/2)
+    ctx.lineTo(posx, posy)
+    ctx.stroke()
+
+ang = (min*6 -90) * (Math.PI/180)
+
+posx = Math.cos(ang)*(clockSize/100*35)+Math.sin(ang)*0;
+posy = Math.sin(ang)*(clockSize/100*35)+Math.cos(ang)*0;
+
+    posx = posx+(clockSize/2);
+    posy = posy+(clockSize/2);
+    ctx.beginPath()
+    ctx.moveTo(clockSize/2,clockSize/2)
+    ctx.lineTo(posx, posy)
+    ctx.stroke()
+
+
+ang = (sec*6 -90) * (Math.PI/180)
+
+posx = Math.cos(ang)*(clockSize/100*40)+Math.sin(ang)*0;
+posy = Math.sin(ang)*(clockSize/100*40)+Math.cos(ang)*0;
+
+    posx = posx+(clockSize/2);
+    posy = posy+(clockSize/2);
+    ctx.strokeStyle = 'red' 
+    ctx.beginPath()
+    ctx.moveTo(clockSize/2,clockSize/2)
+    ctx.lineTo(posx, posy)
+    ctx.stroke()
+    
+    ctx.strokeStyle = 'black' 
+
 }
 
 setInterval(drawtime, 1000)
+
+
+
+ctx.beginPath()
+ctx.arc(clockSize/2,clockSize/2,(clockSize/2),0,2*Math.PI)
+ctx.stroke()
+
+for (let index = 1; index <= 12; index++) {
+    let ang = (index*30 -90) * (Math.PI/180)
+    let posx = Math.cos(ang)*(clockSize/100*44)+Math.sin(ang)*0;
+    let posy = Math.sin(ang)*(clockSize/100*44)+Math.cos(ang)*0;
+
+
+    posx = posx+(clockSize/2);
+    posy = posy+(clockSize/2);  
+    if(posx < (clockSize/2)){
+        posx = posx-22;
+    }
+
+    if(posy>(clockSize/2)){
+        posy = posy+22;
+    }
+
+    ctx.font = "22px Arial"
+    ctx.fillText(index+"",posx, posy)
+}
